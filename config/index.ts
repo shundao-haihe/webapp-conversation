c92d504991a5eea6ba6bacd3cad07ca624870c68
wang haihe <q470118090@gmail.com>
2023-06-28 17:02:37 +0800

    修改了配置文件

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,17 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
-export const API_URL = ''
+export const APP_ID = '7c5fe440-a68c-412f-908e-301fde1ffb88'
+export const API_KEY = 'app-BITiFWkQ2XPzRaxtEmke4HBF'
+export const API_URL = 'https://api.dify.ai/v1'
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'AI易开单-有了易开单业绩翻一番!',
+  description: 'AI易开单帮助你轻松搞定短视频文案，直播话术，抖音，小红书，视频号，促销文案，助力你业绩翻番。',
+  copyright: '顺道数智',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = '我希望你能帮助我提升销售业绩.'
 
 export const API_PREFIX = '/api'
 
